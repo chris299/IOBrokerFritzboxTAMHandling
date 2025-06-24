8ee8031232a75e8e950ab5ee696ed10281b4e6a8
Christoph Winkler <33319001+chris299@users.noreply.github.com>
2025-06-24 11:39:21 +0200

    fixed objekt handling & attribute extraction

--- sendTranskript.js
+++ sendTranskript.js
@@ -4,10 +4,9 @@ var name2, nummer, message;
 on({ id: [].concat(['0_userdata.0.Telefon.Anrufbeantworter.1.Fritzbox_AnrufbeantworterLatestMessageTranskript']), change: 'ne' }, async (obj) => {
   let value = obj.state.val;
   let oldValue = obj.oldState.val;
-  name2 = getAttr(getObject('0_userdata.0.Telefon.Anrufbeantworter.1.Fritzbox_AnrufbeantworterLatestMessageData'), 'Name');
-  nummer = getAttr(getObject('0_userdata.0.Telefon.Anrufbeantworter.1.Fritzbox_AnrufbeantworterLatestMessageData'), 'Number');
+  name2 = getAttr((() => { try { return JSON.parse(getState('0_userdata.0.Telefon.Anrufbeantworter.1.Fritzbox_AnrufbeantworterLatestMessageData').val); } catch (e) { return {}; }})(), 'Name');
+  nummer = getAttr((() => { try { return JSON.parse(getState('0_userdata.0.Telefon.Anrufbeantworter.1.Fritzbox_AnrufbeantworterLatestMessageData').val); } catch (e) { return {}; }})(), 'Number');
   message = ['Transkript der Nachricht von: ',name2,' Nummer : ',nummer,' Nachricht : ',getState('0_userdata.0.Telefon.Anrufbeantworter.1.Fritzbox_AnrufbeantworterLatestMessageTranskript').val].join('');
-  console.info(message);
   sendTo('email.1', 'send', {
     text: message,
     to: 'c@vme.de',
